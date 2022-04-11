1543c5bacc6c4978b676f57b6d601187cd117426
kaylafinkelstein <k.finkelstein4000@gmail.com>
2022-04-11 11:56:50 -0400

    Methods Exercise

---
+++ MethodsExercise2/MyMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise2
+{
+    public class MyMethods
+    {
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Multiply(int num3, int num4)
+        {
+            return num3 * num4;
+
+        }
+        public static int Divide(int num5, int num6)
+        {
+            return num5 / num6;
+        }
+        public static int Subtract(int num7, int num8)
+        {
+            return num7 - num8;
+        }
+    }
+}
---
+++ MethodsExercise2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace MethodsExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+
+            string? userName = Console.ReadLine();
+
+
+            Console.WriteLine("What is your favorite color?");
+
+            string? color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal (plural)?");
+
+            string? animal = Console.ReadLine();
+
+
+            Console.WriteLine("What is your favorite band?");
+
+            string? band = Console.ReadLine();
+
+
+            Console.WriteLine($"My name is {userName}.");
+            Console.WriteLine($"I love {color} {animal}.");
+            Console.WriteLine($"My {color} {animal} and I like to jam out to {band}");
+
+            Console.WriteLine($"Give me a number:");
+
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Give me another number");
+
+            int num2 = int.Parse(Console.ReadLine());
+//#pragma warning restore CS8604 // Possible null reference argument.
+            var mySum = MyMethods.Sum(num1, num2);
+            Console.WriteLine($"The sum of {num1} and {num2} is { mySum }");
+
+
+            Console.WriteLine($"Give me a 3 digit number");
+//#pragma warning disable CS8604 // Possible null reference argument.
+            int num3 = int.Parse(Console.ReadLine());
+//#pragma warning restore CS8604 // Possible null reference argument.
+            Console.WriteLine($"Give me another random number");
+//#pragma warning disable CS8604 // Possible null reference argument.
+            int num4 = int.Parse(Console.ReadLine());
+//#pragma warning restore CS8604 // Possible null reference argument.
+            var myProduct = MyMethods.Multiply(num3, num4);
+            Console.WriteLine($"The product of {num3} and {num4} is { myProduct}");
+
+            Console.WriteLine($"Now, we're going to divide. Give me a number");
+//#pragma warning disable CS8604 // Possible null reference argument.
+            int num5 = int.Parse(Console.ReadLine());
+//#pragma warning restore CS8604 // Possible null reference argument.
+            Console.WriteLine($"Type another number to see your solution");
+//#pragma warning disable CS8604 // Possible null reference argument.
+            int num6 = int.Parse(Console.ReadLine());
+//#pragma warning restore CS8604 // Possible null reference argument.
+            var mySolution = MyMethods.Divide(num5, num6);
+            Console.WriteLine($"The answer to {num5} divided by {num6} is {mySolution}");
+
+            Console.WriteLine($"Lets use subtracrion. Give me any number");
+//#pragma warning disable CS8604 // Possible null reference argument.
+            int num7 = int.Parse(Console.ReadLine());
+//#pragma warning restore CS8604 // Possible null reference argument.
+            Console.WriteLine($"And one last random number");
+//#pragma warning disable CS8604 // Possible null reference argument.
+            int num8 = int.Parse(Console.ReadLine());
+//#pragma warning restore CS8604 // Possible null reference argument.
+            var mySubtract = MyMethods.Subtract(num7, num8);
+            Console.WriteLine($"{num7} minus {num8} is");
+
+            Console.WriteLine($"All your solutions in order should be {mySum}, {myProduct}, {mySolution}, and {mySubtract}");
+
+        }
+    }
+}
